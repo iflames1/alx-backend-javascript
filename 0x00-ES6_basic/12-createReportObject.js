15f1b6a050d03f68ce77a424104c3c347ae8ce6c
Flames <isaacomenuche@gmail.com>
2024-10-01 06:32:58 +0100

    refactor: move getNumberOfDepartments definition out of return statement

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,10 +1,12 @@
 export default function createReportObject(employeesList) {
+  function getNumberOfDepartments(employeesList) {
+    return Object.keys(employeesList).length;
+  }
+
   return {
     allEmployees: {
       ...employeesList,
     },
-    getNumberOfDepartments(employeesList) {
-      return Object.keys(employeesList).length;
-    },
+    getNumberOfDepartments,
   };
 }
