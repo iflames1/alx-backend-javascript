81daabcbc810515a8bde7d417e57bae51d1c3c78
Flames <isaacomenuche@gmail.com>
2024-10-09 08:12:56 +0100

    test: hoisting

--- 0x02-ES6_classes/9-main.js
+++ 0x02-ES6_classes/9-main.js
@@ -1,7 +1,23 @@
-import { listOfStudents } from './9-hoisting';
+import { HolbertonClass, listOfStudents, StudentHolberton } from './9-hoisting';
 
 console.log(listOfStudents);
 
 const listPrinted = listOfStudents.map((student) => student.fullStudentDescription());
 
 console.log(listPrinted);
+
+console.log(
+  'listOfStudents has the correct length:',
+  listOfStudents.length === 5,
+);
+
+console.log(
+  'initialization works correctly:',
+  listOfStudents[0] instanceof StudentHolberton,
+  listOfStudents[0].holbertonClass instanceof HolbertonClass,
+);
+
+console.log(
+  'fullStudentDescription returns the right strings:',
+  listPrinted[0] === 'Guillaume Salva - 2020 - San Francisco',
+);
